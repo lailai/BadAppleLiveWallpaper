db260982150df5a67487b5e43db7189737624e4f
lailai <lai_lai_666@yahoo.co.jp>
2024-07-27 13:15:29 +0900

    Android 14に対応

--- app/src/test/java/net/lailai/android/badapple/livepaper/ExampleUnitTest.kt
+++ app/src/test/java/net/lailai/android/badapple/livepaper/ExampleUnitTest.kt
@@ -1,6 +1,6 @@
 package net.lailai.android.badapple.livepaper
 
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
@@ -11,6 +11,6 @@ import org.junit.Test
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        Assert.assertEquals(4, 2 + 2)
+        assertEquals(4, 2 + 2)
     }
 }
