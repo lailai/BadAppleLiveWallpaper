56c22a69657d983557029f73ff93ddec33f6c677
lailai <lai_lai_666@yahoo.co.jp>
2024-09-28 01:32:58 +0900

    1.3.0 (11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "net.lailai.android.badapple.livepaper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.2.2"
+        versionCode = 11
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
